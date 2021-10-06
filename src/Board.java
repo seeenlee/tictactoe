d7f31600aff70f6b1b2b3969db1b2586ea8719da
seeenlee <naeseel18@gmail.com>
2021-10-06 17:52:13 -0400

    added x and o graphics

--- src/Board.java
+++ src/Board.java
@@ -1,20 +1,22 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Board {
-    private BufferedImage letsplay;
     final int LENGTH = 3;
     private int[][] boardState;
-    private boolean isGameOver = true;
+    private boolean isGameOver = false;
     private boolean player1 = true;
     Scanner s = new Scanner(System.in);
     private int third = 200;
     private boolean starting = true;
+    private static Image letsplay;
+    private static Image x;
+    private static Image o;
+    private boolean start = true;
 
 
     public Board(){
@@ -100,48 +102,87 @@ public class Board {
 
     public void handleClick(MouseEvent me) {
         System.out.println("You just clicked: "+me);
-        //while(!over){
-            if(player1){
-                if (boardState[me.getY() / third][me.getX() / third] == 0){
-                    boardState[me.getY() / third][me.getX() / third] = 1;
-                    player1 = !player1;
-                }
-            }
-            else{
-                if (boardState[me.getY() / third][me.getX() / third] == 0){
+        if (starting) {
+            starting = false;
+        } else {
+            if(!isGameOver) {
+                if(player1){
                     if (boardState[me.getY() / third][me.getX() / third] == 0){
-                        boardState[me.getY() / third][me.getX() / third] = -1;
+                        boardState[me.getY() / third][me.getX() / third] = 1;
                         player1 = !player1;
                     }
                 }
+                else{
+                    if (boardState[me.getY() / third][me.getX() / third] == 0){
+                        if (boardState[me.getY() / third][me.getX() / third] == 0){
+                            boardState[me.getY() / third][me.getX() / third] = -1;
+                            player1 = !player1;
+                        }
+                    }
+                }
+                printGameState();
+                isGameOver = checkWinCon();
+                if (isGameOver){
+                    System.out.println("Game is over!");
+                }
             }
-            printGameState();
-            isGameOver = checkWinCon();
-            if (isGameOver){
-                System.out.println("Game is over!");
-            }
-        //}
+        }
     }
 
     public void tick() {
     }
 
     public void draw(Graphics g) {
-        if (!isGameOver) {
-            for (int row = 0; row < 3; row++) {
-                for (int col = 0; col < 3; col++) {
-                    switch (boardState[row][col]) {
-                        case 0:
-                            break;
-                        case 1:
-                            g.setColor(Color.GREEN);
-                            g.fillRect(col * third, row * third, third, third);
-                            break;
-                        case -1:
-                            g.setColor(Color.RED);
-                            g.fillRect(col * third, row * third, third, third);
+        setUpImages();
+        if (starting) {
+            g.drawImage(letsplay.getScaledInstance(600, 600, Image.SCALE_SMOOTH), 0, 0, null);
+        } else {
+            if (!isGameOver) {
+                for (int row = 0; row < 3; row++) {
+                    for (int col = 0; col < 3; col++) {
+                        switch (boardState[row][col]) {
+                            case 0:
+                                break;
+                            case 1:
+//                                g.setColor(Color.GREEN);
+//                                g.fillRect(col * third, row * third, third, third);
+                                g.drawImage(x.getScaledInstance(200, 200, Image.SCALE_SMOOTH), col * third, row * third, null);
+                                System.out.println("trying");
+                                break;
+                            case -1:
+//                                g.setColor(Color.RED);
+//                                g.fillRect(col * third, row * third, third, third);
+                                g.drawImage(o.getScaledInstance(200, 200, Image.SCALE_SMOOTH), col * third, row * third, null);
+                                break;
+                            default:
+                                System.out.println("Shouldn't be here!");
+                        }
                     }
                 }
+            }
+        }
+    }
+
+    private void setUpImages() {
+        if(letsplay == null) {
+            try {
+                letsplay = ImageIO.read(new File("letsplay.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(x == null) {
+            try {
+                x = ImageIO.read(new File("x.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(o == null) {
+            try {
+                o = ImageIO.read(new File("o.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
